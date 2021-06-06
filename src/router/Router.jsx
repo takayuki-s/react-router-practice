3b4e8f2ff99a0ddf8717378ec852559ae3bbdfbe
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-06 11:24:05 +0900

    ルーティングファイル切り出し完了

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,10 @@
 import { Switch, Route } from "react-router-dom";
 
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
-import { Page2 } from "./Page2";
+import { Home } from "../Home";
+import { Page1 } from "../Page1";
+import { Page1DetailA } from "../Page1DetailA";
+import { Page1DetailB } from "../Page1DetailB";
+import { Page2 } from "../Page2";
 
 export const Router = () => {
   return (
