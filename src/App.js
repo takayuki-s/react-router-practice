5c9f72a3f75ad61c16de2a86595af858d1ea85d2
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-06 10:01:59 +0900

    BrowserRouterライブラリ導入

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Home />
+        <Page1 />
+        <Page2 />
+      </div>
+    </BrowserRouter>
   );
 }
