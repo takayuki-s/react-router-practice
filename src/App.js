212fccd8eec89abcb7e2a299939ef3132cc53b02
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-06 10:12:20 +0900

    SwitchとRouteで選択されたコンポーネント表示 <Route exact path="/">で完全一致させる。exactがないと、"/"を含むパスはHomeと認識されて、どのページを洗濯してもHomeコンポーネントが表示されてしまう。

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
@@ -11,13 +11,21 @@ export default function App() {
       <div className="App">
         <Link to="/">Home</Link>
         <br />
-        <Link to="/Page1">Page1</Link>
+        <Link to="/page1">Page1</Link>
         <br />
-        <Link to="/Page2">Page2</Link>
-        <Home />
-        <Page1 />
-        <Page2 />
+        <Link to="/page2">Page2</Link>
       </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
