892050f9387683e0336fa49e8820b35fd5495f28
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-06 10:04:17 +0900

    Linkライブラリでルーティングパス

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
@@ -9,6 +9,11 @@ export default function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/Page1">Page1</Link>
+        <br />
+        <Link to="/Page2">Page2</Link>
         <Home />
         <Page1 />
         <Page2 />
